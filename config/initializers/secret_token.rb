5a9fae9422ec2fd40e35c0af6a2e08baced1abf5
Tyler Hughes <iampbt@gmail.com>
2012-05-20 14:05:52 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reeltoreel::Application.config.secret_token = 'f68caa90233ca9cb30ca3e14b383611e75d2ac0e827cad20673b0694fd7df9380c5b053198577693318bb5a9432e455e51debecdc3bd26e93a28a57bdcc9a1da'
